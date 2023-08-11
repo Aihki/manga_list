ac043e0124bd1ddb25203f48cf52c2ad4be34654
Aihki <laine.nestori.julius@gmail.com>
2023-08-11 11:23:33 +0300

    small set back

--- js/manga_list.js
+++ js/manga_list.js
@@ -1,13 +1,6 @@
 import { mangaData, infoData } from "./manga_data.js";
 
-
-const infoDataContainer = document.querySelector('.content[data-test3]')
-infoDataContainer.style.display = 'none';
-
-const mangaListContainer = document.querySelector('.manga_list')
-mangaListContainer.style.display = 'none';
-
-function addMangaData(mangaData) {
+function addMangaData(mangaData){
     const mangaListContainer = document.querySelector('.manga_list');
 
 
@@ -33,65 +26,24 @@ function addMangaData(mangaData) {
         mangaListContainer.appendChild(row);
 
     });
-};
-function filterMangaData(searchTerm, filteredMangaData) {
+}
+
+function filterMangaData(searchTerm) {
+    const filterMangaData = mangaData.filter(manga =>
+        manga.series.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     const mangaListContainer = document.querySelector('.manga_list');
     mangaListContainer.innerHTML = '';
 
-    addMangaData(filteredMangaData);
-
-    if (filteredMangaData.length > 0) {
-        mangaListContainer.style.display = 'block';
-    } else {
-        mangaListContainer.style.display = 'none';
-    }
+    addMangaData(filterMangaData);
 }
 
-function populateInfoData(filteredData) {
-    const infoDataContainer = document.querySelector('.content[data-test3]');
-    const dataSetContainers = document.querySelectorAll('.data-set');
-    const descWarpedElements = document.querySelectorAll('.desc_warped');
 
-    filteredData.forEach((data, index) => {
-        const dataSet = dataSetContainers[index];
-        const descWarped = descWarpedElements[index];
 
-        dataSet.querySelector('.type').textContent = data.series;
-        dataSet.querySelector('.value').textContent = `Genre: ${data.genre}, Score: ${data.score}, Format: ${data.format}, Status: ${data.status}, Release: ${data.releasing}, Volumes: ${data.volumes}`;
-        descWarped.textContent = data.description;
-    });
+addMangaData(mangaData)
 
-    if (filteredData.length > 0) {
-        infoDataContainer.style.display = 'block';
-        document.querySelector('[data-test2] h1').textContent = filteredData[0].series;
-        document.querySelector('[data-test2] .description').textContent = filteredData[0].description;
-    } else {
-        infoDataContainer.style.display = 'none';
-        document.querySelector('[data-test2] h1').textContent = '';
-        document.querySelector('[data-test2] .description').textContent = '';
-    }
-}
-
-searchInput.addEventListener('input', function () {
-    const searchTerm = this.value.toLowerCase();
-
-    if (searchTerm.length >= 3) {
-        const filteredInfoData = infoData.filter(data =>
-            data.series.toLowerCase().includes(searchTerm)
-        );
-
-        const filteredMangaData = mangaData.filter(manga =>
-            manga.series.toLowerCase().includes(searchTerm)
-        );
-
-        populateInfoData(filteredInfoData);
-        filterMangaData(searchTerm, filteredMangaData);
-    } else {
-        populateInfoData([]);
-        filterMangaData('');
-    }
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('input', function(){
+    filterMangaData(this.value);
 });
-
-
-addMangaData(mangaData);
-
