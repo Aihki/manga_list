dd88e8e1072ae32639ffead65069ea81068c6b9c
aihki <laine.nestori.julius@gmail.com>
2023-08-22 17:51:32 +0300

    more small chages

--- js/manga_list.js
+++ js/manga_list.js
@@ -1,81 +1,87 @@
-import { mangaData } from './manga_data.js';
+import { mangaData } from "./manga_data.js";
 
 function populateMangaDetails(index) {
-    console.log('populateMangaDetails called with index:', index);
-    const mangaTitle = document.querySelector('.content h1');
-    const mangaDescription = document.querySelector('.content .description');
-    const mangaDescriptionMobile = document.querySelector('.warped .desc_warped')
-    const mangaType = document.querySelectorAll('.data-set .type');
-    const mangaValue = document.querySelectorAll('.data-set .value');
-    const mangaTags = document.querySelector('.tags');
-    const mangaVolumes = document.querySelector('.list_content .manga');
+  console.log("populateMangaDetails called with index:", index);
+  const mangaTitle = document.querySelector(".content h1");
+  const mangaDescription = document.querySelector(".content .description");
+  const mangaDescriptionMobile = document.querySelector(".warped .desc_warped");
+  const mangaType = document.querySelectorAll(".data-set .type");
+  const mangaValue = document.querySelectorAll(".data-set .value");
+  const mangaTags = document.querySelector(".tags");
+  const mangaVolumes = document.querySelector(".list_content .manga");
 
-    mangaTitle.textContent = '';
-    mangaDescription.textContent = '';
-    mangaDescriptionMobile.textContent = '';
-    mangaTags.textContent = '';
-    mangaVolumes.textContent = '';
+  mangaTitle.textContent = "";
+  mangaDescription.textContent = "";
+  mangaDescriptionMobile.textContent = "";
+  mangaTags.textContent = "";
+  mangaVolumes.textContent = "";
 
+  mangaTitle.textContent = mangaData[index].series;
+  mangaDescription.textContent = mangaData[index].description;
 
+  mangaDescriptionMobile.textContent = mangaData[index].description;
 
-    mangaTitle.textContent = mangaData[index].series;
-    mangaDescription.textContent = mangaData[index].description;
+  mangaType[0].textContent = "Score:";
+  mangaValue[0].textContent = mangaData[index].score;
 
-    mangaDescriptionMobile.textContent = mangaData[index].description;
+  mangaType[1].textContent = "Format:";
+  mangaValue[1].textContent = mangaData[index].format;
 
-    mangaType[0].textContent = 'Score:';
-    mangaValue[0].textContent = mangaData[index].score;
+  mangaType[2].textContent = "Status:";
+  mangaValue[2].textContent = mangaData[index].status;
 
-    mangaType[1].textContent = 'Format:';
-    mangaValue[1].textContent = mangaData[index].format;
+  mangaType[3].textContent = "Releasing:";
+  mangaValue[3].textContent = mangaData[index].releasing;
 
-    mangaType[2].textContent = 'Status:';
-    mangaValue[2].textContent = mangaData[index].status;
+  mangaData[index].genre.forEach((genre) => {
+    const genreTag = document.createElement("div");
+    genreTag.classList.add("tag");
+    genreTag.textContent = genre;
+    mangaTags.appendChild(genreTag);
+  });
 
-    mangaType[3].textContent = 'Releasing:';
-    mangaValue[3].textContent = mangaData[index].releasing;
+  mangaData[index].volumesInfo.forEach((volumeInfo) => {
+    const mangaVolume = document.createElement("div");
+    mangaVolume.classList.add("manga", "row");
 
-    mangaData[index].genre.forEach(genre => {
-        const genreTag = document.createElement('div');
-        genreTag.classList.add('tag');
-        genreTag.textContent = genre;
-        mangaTags.appendChild(genreTag);
-    });
+    const volumeDiv = document.createElement("div");
+    volumeDiv.classList.add("volume");
+    volumeDiv.textContent = volumeInfo.volume;
+    mangaVolume.appendChild(volumeDiv);
 
-    mangaData[index].volumesInfo.forEach(volumeInfo => {
-        const mangaVolume = document.createElement('div');
-        mangaVolume.classList.add('manga', 'row');
-        mangaVolume.innerHTML = `
-            <div class="volume">${volumeInfo.volume}</div>
-            <div class="owned">${volumeInfo.owned ? 'Yes' : 'No'}</div>
-            <div class="readed">${volumeInfo.readed ? 'Yes' : 'No'}</div>
-        `;
-        mangaVolumes.appendChild(mangaVolume);
-    });
+    const ownedDiv = document.createElement("div");
+    ownedDiv.classList.add("owned");
+    ownedDiv.textContent = volumeInfo.owned ? "Yes" : "No";
+    mangaVolume.appendChild(ownedDiv);
+
+    const readedDiv = document.createElement("div");
+    readedDiv.classList.add("readed");
+    readedDiv.textContent = volumeInfo.readed ? "Yes" : "No";
+    mangaVolume.appendChild(readedDiv);
+
+    mangaVolumes.appendChild(mangaVolume);
+  });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const searchInput = document.getElementById("searchInput");
 
+  searchInput.addEventListener("input", () => {
+    const searchInputValue = searchInput.value.trim();
 
-document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.getElementById('searchInput');
-
-    searchInput.addEventListener('input', () => {
-        const searchInputValue = searchInput.value.trim();
-
-        if (searchInputValue === '') {
-            // Clear details or hide them when search input is empty
-            // For example: clearDetails();
-        } else {
-            const index = mangaData.findIndex(manga => manga.series.toLowerCase() === searchInputValue.toLowerCase());
-            if (index !== -1) {
-                populateMangaDetails(index);
-            } else {
-                // Clear details or show a message if manga not found
-                // For example: populateMangaDetails(-1);
-
-            }
-        }
-    });
+    if (searchInputValue === "") {
+      // Clear details or hide them when search input is empty
+      // For example: clearDetails();
+    } else {
+      const index = mangaData.findIndex(
+        (manga) => manga.series.toLowerCase() === searchInputValue.toLowerCase()
+      );
+      if (index !== -1) {
+        populateMangaDetails(index);
+      } else {
+        // Clear details or show a message if manga not found
+        // For example: populateMangaDetails(-1);
+      }
+    }
+  });
 });
-
-
